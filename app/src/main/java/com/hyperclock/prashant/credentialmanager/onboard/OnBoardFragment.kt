cfb2cf7135d639677513ab6cfdd77ee7939a4e95
Prashant <prashantsn23@gmail.com>
2019-08-10 18:43:50 +0530

    Add Custom login screen without the textview

--- app/src/main/java/com/hyperclock/prashant/credentialmanager/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/hyperclock/prashant/credentialmanager/onboard/OnBoardFragment.kt
@@ -17,8 +17,9 @@ import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.hyperclock.prashant.credentialmanager.databinding.FragmentOnBoardBinding
 import android.content.Context
+import android.util.Log
 import android.view.inputmethod.InputMethodManager
-
+import android.widget.ProgressBar
 
 class OnBoardFragment : Fragment() {
 
@@ -48,7 +49,37 @@ class OnBoardFragment : Fragment() {
         })
 
         viewmodel.randomString.observe(this, Observer { pass->
-            binding.hintText.text = pass.toString()
+            //binding.hintText.text = pass.toString()
+            val layout = binding.linearLayout
+
+            val colors = listOf(
+                android.R.color.holo_red_dark,
+                android.R.color.holo_blue_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_purple,
+                android.R.color.holo_green_dark,
+                android.R.color.darker_gray,
+                android.R.color.holo_orange_light,
+                android.R.color.black,
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_blue_bright
+            )
+
+            for (i in 0..pass.size-1){
+
+                val progressBar = ProgressBar(activity, null, android.R.attr.progressBarStyleHorizontal)
+                progressBar.apply{
+                    //scaleX = (pass.indexOf(i)*0.9).toFloat()
+                    max = 10
+                    //setLayoutParams(ViewGroup.LayoutParams(layout.width, layout.height))
+                    isIndeterminate = false
+                    setBackgroundColor(colors[i])
+                    //progressDrawable = resources.getDrawable(com.hyperclock.prashant.credentialmanager.R.drawable.circle)
+                    progress = pass[i]
+                    Log.d("OnBoardFragment","value is $i with progress $progress")
+                }
+                layout.addView(progressBar)
+            }
         })
 
         (activity as AppCompatActivity).supportActionBar?.title = getString(com.hyperclock.prashant.credentialmanager.R.string.login_string)
